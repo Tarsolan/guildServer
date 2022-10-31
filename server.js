baa3970ac622663c1d47a11e40b9f8b55d1230b0
Tarsolan <aridgeley@msn.com>
2022-10-31 11:48:06 -0230

    security updates

--- server.js
+++ server.js
@@ -51,14 +51,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const access_control = process.env.SITE_URL;
 global.DEBUG = true;
 
 var cors = require("cors");
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: SITE_URL }));
 
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", access_control);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
