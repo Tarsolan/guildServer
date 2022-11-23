07cfdb4bc5d6d01591fc15e5cb46f6572b908b6a
Tarsolan <aridgeley@msn.com>
2022-11-23 12:24:39 -0330

    switched to postgres search

--- database/searchMission.dal.js
+++ database/searchMission.dal.js
@@ -1,29 +1,23 @@
-const { client } = require("./mongo");
+const db = require("./pgAdmin");
 
 const searchMission = async (body) => {
   const { searchText } = body;
-  await client.connect();
-  const cursor = client
-    .db("Guild")
-    .collection("missions")
-    .find({ $text: { $search: searchText } });
-  const results = await cursor.toArray();
-  // console.log(results);
+  let sql = `SELECT * FROM part2.mission
+  WHERE to_tsvector(job_description) @@ to_tsquery($1);`;
 
-  return results;
+  let res = await db.query(sql, [searchText]);
+  //console.log(res.rows);
+  return res.rows;
 };
 
 const searchMissionReport = async (body) => {
   const { searchText } = body;
-  await client.connect();
-  const cursor = client
-    .db("Guild")
-    .collection("mission_reports")
-    .find({ $text: { $search: searchText } });
-  const results = await cursor.toArray();
-  // console.log(results);
+  let sql = `SELECT * FROM part2.mission_report
+  WHERE to_tsvector(report_details) @@ to_tsquery($1);`;
 
-  return results;
+  let res = await db.query(sql, [searchText]);
+  //console.log(res.rows);
+  return res.rows;
 };
 
 module.exports = { searchMission, searchMissionReport };
